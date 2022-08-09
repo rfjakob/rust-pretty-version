725c170d6bdaefd50ba5c48b31adf9458eb0e9bf
Jakob Unterwurzacher <jakobunt@gmail.com>
2022-08-09 23:12:14 +0200

    Use a macro to simplify things for the caller

--- example-print/src/main.rs
+++ example-print/src/main.rs
@@ -1,5 +1,5 @@
 use pretty_version::pretty_version;
 
 fn main() {
-    println!("{}", pretty_version(env!("CARGO_PKG_VERSION")));
+    println!("{}", pretty_version!());
 }
--- pretty-version/src/lib.rs
+++ pretty-version/src/lib.rs
@@ -1,27 +1,32 @@
-use git_version::git_describe;
+//! pretty_version()! returns a version string (type &str)
+//! that looks like `0.9.4.234-07f1468-dirty`.
+//!
+//! Parts:
+//!
+//!     0.9.4 ..... version from Cargo.toml
+//!     234 ....... Jenkins BUILD_NUMBER or 0 if unset
+//!     07f1468 ... abbreviated git commit hash
+//!     -dirty .... added only if there are uncommited changes
+//!
+//! Usage:
+//!
+//!     use pretty_version::pretty_version;
+//!     fn main() {
+//!         println!("{}", pretty_version!());
+//!     }
 
-/// Returns the a version string that looks like `0.9.4.234-07f1468-dirty`.
-///
-/// Parts:
-///
-///     0.9.4 ..... version from Cargo.toml
-///     234 ....... Jenkins BUILD_NUMBER or 0 if unset
-///     07f1468 ... abbreviated git commit hash
-///     dirty ..... added only if there are uncommited changes
-///
-/// Usage:
-///
-///     use pretty_version::pretty_version;
-///     fn main() {
-///         println!("{}", pretty_version(env!("CARGO_PKG_VERSION")));
-///     }
-///
-/// `env!("CARGO_PKG_VERSION")` has to be passed by the caller, as otherwise
-/// it would expand to the version of the pretty-version crate itself.
-pub fn pretty_version(cargo_pkg_version: &str) -> String {
-	return format!("{}.{}-{}", 
-		cargo_pkg_version,
-		option_env!("BUILD_NUMBER").unwrap_or("0"),
-		// empty "--match" disables all tags and forces output of shorthash
-		git_describe!("--always", "--match=", "--dirty"));
+/// Returns a version string like `0.9.4.234-07f1468-dirty` built
+/// from the crate version, Jenkins build number and the git shorthash.
+#[macro_export]
+macro_rules! pretty_version{
+	() => {
+		format!("{}.{}-{}",
+			env!("CARGO_PKG_VERSION"),
+			option_env!("BUILD_NUMBER").unwrap_or("0"),
+			// empty "--match" disables all tags and forces output of shorthash
+			pretty_version::git_describe!("--always", "--match=", "--dirty")).as_str()
+    };
 }
+
+#[doc(hidden)]
+pub use git_version::git_describe;
